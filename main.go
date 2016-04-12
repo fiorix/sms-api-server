9509e39d0df463eec1bb45a8301649e58cfc9082
Alexandre Fiori <fiorix@gmail.com>
2016-04-12 18:05:38 -0400

    Add support for client certificates
    Bump version to 1.2

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ package main
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
@@ -24,34 +26,31 @@ import (
 )
 
 // Version of this server.
-var Version = "v1.1"
+var Version = "v1.2"
+
+type Opts struct {
+	ListenAddr        string
+	APIPrefix         string
+	PublicDir         string
+	Log               bool
+	LogTS             bool
+	CAFile            string
+	CertFile          string
+	KeyFile           string
+	SMPPAddr          string
+	ClientTLS         bool
+	ClientTLSInsecure bool
+	ShowVersion       bool
+}
 
 func main() {
-	laddr := flag.String("http", ":8080", "host:port to listen on")
-	public := flag.String("public", "", "public dir to serve under \"/\", optional")
-	logreq := flag.Bool("log", false, "log http requests to stderr")
-	certf := flag.String("cert", "", "ssl certificate file for http server, optional")
-	keyf := flag.String("key", "", "ssl key file for http server, optional")
-	prefix := flag.String("prefix", "/", "prefix for http endpoints")
-	cliaddr := flag.String("smpp", "localhost:2775", "host:port of the smsc to connect to via smpp 3.4")
-	clitls := flag.Bool("tls", false, "connect to smsc using tls")
-	version := flag.Bool("version", false, "show version and exit")
-	cliprecaire := flag.Bool("precaire", false, "accept invalid ssl certificate from smsc")
-	flag.Usage = func() {
-		fmt.Printf("Usage: [env] %s [options]\n", os.Args[0])
-		fmt.Printf("Environment variables:\n")
-		fmt.Printf(" SMPP_USER: username for smpp client connection\n")
-		fmt.Printf(" SMPP_PASSWD: password for smpp client connection\n")
-		fmt.Printf("Options:\n")
-		flag.PrintDefaults()
-	}
-	flag.Parse()
-	if *version {
+	o := ParseOpts()
+	if o.ShowVersion {
 		fmt.Println("sms-api-server", Version)
 		os.Exit(0)
 	}
 	tx := &smpp.Transceiver{
-		Addr:   *cliaddr,
+		Addr:   o.SMPPAddr,
 		User:   os.Getenv("SMPP_USER"),
 		Passwd: os.Getenv("SMPP_PASSWD"),
 	}
@@ -62,40 +61,86 @@ func main() {
 		tx.Close()
 		os.Exit(0)
 	}()
-	if *clitls {
+	if o.ClientTLS {
 		host, _, _ := net.SplitHostPort(tx.Addr)
 		tx.TLS = &tls.Config{
 			ServerName: host,
 		}
-		if *cliprecaire {
+		if o.ClientTLSInsecure {
 			tx.TLS.InsecureSkipVerify = true
 		}
 	}
-	api := &apiserver.Handler{
-		Prefix: *prefix,
-		Tx:     tx,
-	}
+	api := &apiserver.Handler{Prefix: o.APIPrefix, Tx: tx}
 	conn := api.Register(http.DefaultServeMux)
 	go func() {
 		for c := range conn {
 			m := fmt.Sprintf("SMPP connection status to %s: %s",
-				*cliaddr, c.Status())
+				o.SMPPAddr, c.Status())
 			if err := c.Error(); err != nil {
 				m = fmt.Sprintf("%s (%v)", m, err)
 			}
 			log.Println(m)
 		}
 	}()
-	if *public != "" {
-		http.Handle("/", http.StripPrefix(*prefix,
-			http.FileServer(http.Dir(*public))))
+	if o.PublicDir != "" {
+		fs := http.FileServer(http.Dir(o.PublicDir))
+		http.Handle("/", http.StripPrefix(o.APIPrefix, fs))
 	}
 	mux := http.Handler(http.DefaultServeMux)
-	if *logreq {
+	if o.Log {
+		if !o.LogTS {
+			log.SetFlags(0)
+		}
 		mux = handlers.LoggingHandler(os.Stderr, mux)
 	}
-	if *certf == "" || *keyf == "" {
-		log.Fatal(http.ListenAndServe(*laddr, mux))
+	err := ListenAndServe(o, mux)
+	if err != nil {
+		log.Fatal(err)
 	}
-	log.Fatal(http.ListenAndServeTLS(*laddr, *certf, *keyf, mux))
+}
+
+func ParseOpts() *Opts {
+	o := &Opts{ListenAddr: ":8080", SMPPAddr: "localhost:2775", LogTS: true}
+	flag.StringVar(&o.ListenAddr, "http", o.ListenAddr, "host:port to listen on for http or https")
+	flag.StringVar(&o.APIPrefix, "prefix", o.APIPrefix, "prefix for http(s) endpoints")
+	flag.StringVar(&o.PublicDir, "public", o.PublicDir, "public dir to serve under \"/\", optional")
+	flag.BoolVar(&o.Log, "log", o.Log, "log http requests")
+	flag.BoolVar(&o.LogTS, "log-timestamp", o.LogTS, "add timestamp to logs")
+	flag.StringVar(&o.CAFile, "ca", o.CAFile, "x509 CA certificate file (for client auth)")
+	flag.StringVar(&o.CertFile, "cert", o.CertFile, "x509 certificate file for https server")
+	flag.StringVar(&o.KeyFile, "key", o.KeyFile, "x509 key file for https server")
+	flag.StringVar(&o.SMPPAddr, "smpp", o.SMPPAddr, "host:port of the SMSC to connect to via SMPP v3.4")
+	flag.BoolVar(&o.ClientTLS, "tls", o.ClientTLS, "connect to SMSC using TLS")
+	flag.BoolVar(&o.ClientTLSInsecure, "precaire", o.ClientTLSInsecure, "disable TLS checks for client connection")
+	flag.BoolVar(&o.ShowVersion, "version", o.ShowVersion, "show version and exit")
+	flag.Usage = func() {
+		fmt.Printf("Usage: [env] %s [options]\n", os.Args[0])
+		fmt.Printf("Environment variables:\n")
+		fmt.Printf(" SMPP_USER: username for smpp client connection\n")
+		fmt.Printf(" SMPP_PASSWD: password for smpp client connection\n")
+		fmt.Printf("Options:\n")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	return o
+}
+
+func ListenAndServe(o *Opts, f http.Handler) error {
+	s := &http.Server{Addr: o.ListenAddr, Handler: f}
+	if o.CertFile == "" || o.KeyFile == "" {
+		return s.ListenAndServe()
+	}
+	if o.CAFile != "" {
+		b, err := ioutil.ReadFile(o.CAFile)
+		if err != nil {
+			return err
+		}
+		cp := x509.NewCertPool()
+		cp.AppendCertsFromPEM(b)
+		s.TLSConfig = &tls.Config{
+			ClientCAs:  cp,
+			ClientAuth: tls.RequireAndVerifyClientCert,
+		}
+	}
+	return s.ListenAndServeTLS(o.CertFile, o.KeyFile)
 }
